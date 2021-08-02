71119385575562f0c83846fdef5e280716888bee
Mohammad Muneer <mohammadesk42@gmail.com>
2021-08-03 01:10:48 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,66 @@
+from sqlalchemy import Column, String, Integer, DateTime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import json
+import os
 
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key = True)
+    title = Column(String, nullable=False)
+    releaseDate = Column(DateTime(), nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.releaseDate
+        }
