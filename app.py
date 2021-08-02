9fc3863713b7625868cca76fbe185d7d66149415
Mohammad Muneer <mohammadesk42@gmail.com>
2021-08-03 01:17:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import Actor, Movie, db, setup_db
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
 
   return app
