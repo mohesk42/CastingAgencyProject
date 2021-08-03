010f20176baa33e4403ff2b2865ee2742b9453dc
Mohammad Muneer <mohammadesk42@gmail.com>
2021-08-03 20:39:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
     return response
 
   @app.route('/actors', methods=['GET'])
-  def get_actors():
+  def get_actors(jwt):
     actors = Actor.query.all()
     
     actorsList = []
@@ -33,7 +33,7 @@ def create_app(test_config=None):
       }), 200
   
   @app.route('/movies', methods=['GET'])
-  def get_movies():
+  def get_movies(jwt):
     movies = Movie.query.all()
     
     moviesList = []
@@ -49,7 +49,7 @@ def create_app(test_config=None):
       }), 200
   
   @app.route('/actors/<int:actor_id>', methods=['DELETE'])
-  def delete_actor(actor_id):
+  def delete_actor(jwt, actor_id):
     try:
       actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
       if actor is None:
@@ -64,7 +64,7 @@ def create_app(test_config=None):
       abort(422)
   
   @app.route('/movies/<int:movie_id>', methods=['DELETE'])
-  def delete_movie(movie_id):
+  def delete_movie(jwt, movie_id):
     try:
       movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
       if movie is None:
@@ -77,7 +77,79 @@ def create_app(test_config=None):
       })
     except:
       abort(422)
+  
+  @app.route('/actors', methods=['POST'])
+  def create_actor(jwt):
+    body = request.get_json()
 
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
+
+    try:
+      actor = Actor(name=name, age=age, gender=gender)
+      actor.insert()
+      return jsonify({
+        'success': True,
+        'actor': actor.format()
+      }), 201
+    except:
+      abort(422)
+  
+  @app.route('/movies', methods=['POST'])
+  def create_movie(jwt):
+    body = request.get_json()
+
+    title = body.get('title', None)
+    releaseDate = body.get('release_date', None)
+
+    try:
+      movie = Movie(title=title, releaseDate=releaseDate)
+      movie.insert()
+      return jsonify({
+        'success': True,
+        'movie': movie.format()
+      }), 201
+    except:
+      abort(422)
+
+  @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+  def edit_actor(jwt, actor_id):
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+    if actor is None:
+      abort(404)
+    
+    body = request.get_json()
+    if 'name' in body:
+      actor.name = body.get('name', None)
+    if 'age' in body:
+      actor.age = body.get('age', None)
+    if 'gender' in body:
+      actor.gender = body.get('gender', None)
+    
+    actor.update()
+    return jsonify({
+      'success': True,
+      'actor': actor.format()
+    }), 200
+
+  @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+  def edit_movie(jwt, movie_id):
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+    if movie is None:
+      abort(404)
+    
+    body = request.get_json()
+    if 'title' in body:
+      movie.title = body.get('title', None)
+    if 'release_date' in body:
+      movie.releaseDate = body.get('release_date', None)
+    
+    movie.update()
+    return jsonify({
+      'success': True,
+      'movie': movie.format()
+    }), 200
 
   return app
 
