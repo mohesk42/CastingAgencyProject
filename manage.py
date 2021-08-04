01881fffe7734d8d7de85a88e0bdb5993cdbf93b
Mohammad Muneer <mohammadesk42@gmail.com>
2021-08-04 21:25:11 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+from os import name
 from app import create_app
 from models import Actor, Movie, db
 from flask_script import Manager
@@ -7,6 +8,18 @@ app = create_app()
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def seed():
+    actor1 = Actor(name='Nemo', age='29', gender='Female')
+    actor2 = Actor(name='Basil', age='22', gender='Male')
+    actor1.insert()
+    actor2.insert()
+
+    movie1 = Movie(title='Ice Land', releaseDate='2020/1/1')
+    movie2 = Movie(title='Fire', releaseDate='2014/4/3')
+    movie1.insert()
+    movie2.insert()
 
 if __name__ == '__main__':
     manager.run()
